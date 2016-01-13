b43eea88cb63f33cc89fd49649386e9a28260f5a
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-13 17:19:59 +0000

    enforces single responsibility principle

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -4,10 +4,13 @@ class FizzBuzz(object):
     def play(self, number):
         if isinstance(number, basestring):
             raise ValueError
-        if number % 15 == 0:
+        if self._is_divisible_by(number, 15):
             return 'FizzBuzz'
-        if number % 3 == 0:
+        if self._is_divisible_by(number, 3):
             return 'Fizz'
-        if number % 5 == 0:
+        if self._is_divisible_by(number, 5):
             return 'Buzz'
         return number
+
+    def _is_divisible_by(self, number, divisor):
+        return number % divisor == 0
