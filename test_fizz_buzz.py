0bbd1a4f5e83f2c0bf3d3a493c415b2d381a14ac
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-13 16:53:34 +0000

    refactors

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -9,7 +9,7 @@ class FizzBuzzTestCase(unittest.TestCase):
     def test_play_delivers_fizz_with_multiple_of_3(self):
         self.assertEqual(self.fizz_buzz.play(3), 'Fizz')
 
-    def test_play_accepts_only_numbers(self):
+    def test_play_does_not_accept_strings(self):
         self.assertRaises(ValueError, self.fizz_buzz.play, 'two')
 
 
